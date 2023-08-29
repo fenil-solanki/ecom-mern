54558b33604749138c6b8691a84fe84031d0b830
Sahil-Rana-990 <tyuir8098@gmail.com>
2023-08-29 12:47:10 +0530

    basic mern

---
+++ server/server.js
@@ -0,0 +1,94 @@
+
+/** source/server.ts */
+const http=require("http")
+const express=require("express")
+const morgan=require("morgan")
+// import express, { Express } from 'express';
+// import morgan from 'morgan';
+// const app=require("./source/routes/posts.js");
+require("./source/db/conn.js")
+const cors=require("cors")
+const app = express();
+/** Logging */
+app.use(morgan('dev'));
+/** Parse the request */
+app.use(express.urlencoded({ extended: false }));
+/** Takes care of JSON data */
+app.use(express.json({limit:"10mb"}));
+
+app.use(cors())
+var bodyParser = require('body-parser');
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.urlencoded({limit:"10mb",extended:true,parameterLimit:50000}))
+
+// app.set("view engine","ejs")
+// app.set('views', __dirname + '/views')
+app.use(bodyParser.json());
+
+
+/** RULES OF OUR API */
+// app.use((req, res, next):any => {
+//     // set the CORS policy
+//     res.header('Access-Control-Allow-Origin', '*');
+//     // set the CORS headers
+//     res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+//     // set the CORS method headers
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
+
+/** Routes */
+const controller=require("./source/controllers/posts.js")
+app.get('/', controller.home);
+app.get('/finduser', controller.finduser);
+app.get('/finduseremail', controller.finduserByemail);
+
+app.post('/adduser',controller.adduser)
+app.post('/upload',async(req,res)=>{
+    const tempDate=Date.now()
+    const ImageDataStored = ImageModel({
+      imgName: tempDate,
+      imgData: req.body.imageData,
+      joindate:tempDate
+    });
+    await ImageDataStored.save().then((result) => {
+      console.log("image stored!!");
+      res.send({ message: tempDate });
+    });
+    
+})
+app.get('/images/:imgname',async(req,res)=>{
+        const ImageObj = await ImageModel.findOne({ imgName: req.params.imgname });
+        console.log(ImageObj)
+        const imgdata = ImageObj.imgData.split(",")[1]; // Extract the base64 data part
+        const imageBuffer = Buffer.from(imgdata, "base64");
+      
+        res.setHeader("Content-Type", "image/png");
+        res.end(imageBuffer);
+})
+
+// app.put('/updatecart',controller.updateCart)
+app.post('/postproduct',controller.postProduct)
+app.get('/products',controller.getProducts)
+app.get('/products/:id',controller.getSingleProduct)
+
+
+/** Error handling */
+// app.use((req:any, res:any, next:any) => {
+//     const error = new Error('not found');
+//     return res.status(404).json({
+//         message: error.message
+//     });
+// });
+
+/** Server */
+const httpServer = http.createServer(app);
+const PORT = process.env.PORT ?? 5000;
+httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
