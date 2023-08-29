041d60b0e33255d76c54b8aa60344d83cd28215a
fenil-solanki <114174167+fenil-solanki@users.noreply.github.com>
2023-08-29 15:19:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -46,10 +46,10 @@ let ProductObj=require("./source/models/Product.js")
 let ImageModel=require("./source/models/Image.js")
 
 app.use(express.json({limit:"10mb"}));
-app.use(bodyParser.urlencoded({
-    extended: true,
-    limit:"10mb"
-}));
+// app.use(bodyParser.urlencoded({
+//     extended: true,
+//     limit:"10mb"
+// }));
 app.use(express.urlencoded({limit:"10mb",extended:true,parameterLimit:50000}))
 
 app.use(cors({
