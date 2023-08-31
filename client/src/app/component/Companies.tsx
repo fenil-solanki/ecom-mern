8605dc147d70cb3552a136f2b407808050be2573
fenil-solanki <114174167+fenil-solanki@users.noreply.github.com>
2023-08-31 17:45:48 +0530

    Update Companies.tsx

--- client/src/app/component/Companies.tsx
+++ client/src/app/component/Companies.tsx
@@ -6,8 +6,8 @@ const Companies = ({company}:any) => {
     const {updateFilterValue}=useFilterContext()
     const capitalcompany = company.charAt(0).toUpperCase() + company.slice(1);
   return (
-    <button name="company" value={company} className="w-full font-semibold text-left py-2 grid grid-cols-2" style={{color:"#8A8989"}} onClick={updateFilterValue}>
-    {capitalcompany}
+    <button name="company" value={company} className="w-full font-semibold text-left py-1 grid grid-cols-4 text-xs sm:text-sm sm:py-2 md:text-base lg:text-xl" style={{color:"#8A8989"}} onClick={updateFilterValue}>
+    <span className='col-span-3'>{capitalcompany}</span>
     <MdOutlineArrowForwardIos className='inline text-right'></MdOutlineArrowForwardIos>
     </button>
   )
