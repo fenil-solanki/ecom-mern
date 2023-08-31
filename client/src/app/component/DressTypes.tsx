e0588197f8b28dff9c039afd5615997d6334e940
fenil-solanki <114174167+fenil-solanki@users.noreply.github.com>
2023-08-31 18:04:20 +0530

    Update DressTypes.tsx

--- client/src/app/component/DressTypes.tsx
+++ client/src/app/component/DressTypes.tsx
@@ -6,10 +6,13 @@ const DressTypes = ({type}:any) => {
     const {updateFilterValue}=useFilterContext()
     const capitalType = type.charAt(0).toUpperCase() + type.slice(1);
   return (
-    <button name="type" value={type} className="w-full font-semibold text-left py-1 grid grid-cols-4 text-xs sm:text-sm sm:py-2 md:text-base lg:text-xl" style={{color:"#8A8989"}} onClick={updateFilterValue}>
-    <span className='col-span-3'>{capitalType}</span>
-    <MdOutlineArrowForwardIos className='inline text-right'></MdOutlineArrowForwardIos>
+    <div className='w-full  grid grid-cols-4 py-1 sm:py-2 '>
+    <button name="type" value={type} className="col-span-3 font-semibold text-left text-xs sm:text-sm md:text-base lg:text-xl" style={{color:"#8A8989"}} onClick={updateFilterValue}>
+    {capitalType}
     </button>
+    <MdOutlineArrowForwardIos className='inline text-right font-semibold text-xs sm:text-sm md:text-base  lg:text-xl' style={{color:"#8A8989"}} ></MdOutlineArrowForwardIos>
+
+    </div>
   )
 }
 
