0b7b1536602e1c5137a79b59f7beed8786c7fb65
fenil-solanki <114174167+fenil-solanki@users.noreply.github.com>
2023-09-01 12:51:06 +0530

    Update page.tsx

--- client/src/app/singleproduct/[productid]/page.tsx
+++ client/src/app/singleproduct/[productid]/page.tsx
@@ -14,7 +14,7 @@ import {TiTick} from 'react-icons/ti'
 import { FaMinus, FaPlus } from 'react-icons/fa'
 import { useUserContext } from '@/context/UserContext'
 import RatingStar from '@/app/component/RatingStar'
-const Page = () => {
+const page = () => {
     const pathname=usePathname()
     const url=pathname
     const substringToRemove = '/singleproduct/';
@@ -97,16 +97,16 @@ const Page = () => {
             <div className='col-span-6'>
               <SingleProductImages images={singleProduct.image}></SingleProductImages>
             </div>
-            <div className='col-span-6 p-14'>
-              <p style={{color:"#807D7E"}}>Shop{'>'}Women{'>'}Top</p>
-            <h3 className='font-semibold text-4xl my-6'>{singleProduct.name} {singleProduct.company}</h3>
+            <div className='col-span-6 pt-14 pb-12 px-10'>
+              <p style={{color:"#807D7E"}} className='text-base lg:text-lg'>Shop{'>'}Women{'>'}Top</p>
+            <h3 className='font-medium text-2xl my-3.5 lg:text-3xl lg:font-semibold lg:my-4'>{singleProduct.name} {singleProduct.company}</h3>
             <div>
                
               <RatingStar stars={singleProduct.stars}></RatingStar>
 
               
-              <span style={{color:"#807D7E"}}>{singleProduct.stars}</span>
-              <div className='inline'>
+              <span style={{color:"#807D7E"}} className='text-sm lg:text-base'>{singleProduct.stars}</span>
+              <div className='inline text-sm lg:text-base'>
               <BiCommentDetail className='inline' style={{color:"#807D7E"}}></BiCommentDetail>
              <span style={{color:"#807D7E"}}>{singleProduct.reviews} comments</span> 
               </div>
@@ -114,15 +114,15 @@ const Page = () => {
             </div>
     
     
-            <div className='w-[50%] my-14'>
-              <p className='inline' style={{color:"#3F4646"}}>Select Size</p>
-              <p className='inline' style={{color:"#807D7E"}}>Size Guide</p>
+            <div className='w-[100%] xl:w-[80%] 2xl:w-[50%] my-6 lg:my-8'>
+              <p className='inline text-lg lg:text-xl' style={{color:"#3F4646"}}>Select Size</p>
+              <p className='inline text-lg ml-4 lg:text-xl' style={{color:"#807D7E"}}>Size Guide</p>
     
-              <div className='my-10'>
+              <div className='my-6 lg:my-10'>
                 {
                    singleProduct.sizes !== undefined && singleProduct.sizes.length !==0 && singleProduct.sizes?
                   singleProduct.sizes.map((curSize:any)=>{
-                    return  curSize===selectedSize?<button key={curSize} className='border-2 bored-[#BEBCBD] rounded rounded-lg inline m-2 w-10 h-10 border border-[#000] bg-[#000]'onClick={()=>setselectedSize(curSize)} style={{color:"#fff"}}>{curSize}</button>:<button key={curSize} className='border-2 bored-[#BEBCBD] rounded rounded-lg inline m-2 w-10 h-10 border border-[#000]'onClick={()=>setselectedSize(curSize)}>{curSize}</button>
+                    return  curSize===selectedSize?<button key={curSize} className='border-2 bored-[#BEBCBD] rounded rounded-lg inline m-2 h-8 w-8 text-base lg:text-xl lg:w-10 lg:h-10 border border-[#000] bg-[#000]'onClick={()=>setselectedSize(curSize)} style={{color:"#fff"}}>{curSize}</button>:<button key={curSize} className='border-2 bored-[#BEBCBD] rounded rounded-lg inline m-2 h-8 w-8 text-base lg:text-xl lg:w-10 lg:h-10 border border-[#000]'onClick={()=>setselectedSize(curSize)}>{curSize}</button>
                   }):null
                 }
           
@@ -131,8 +131,8 @@ const Page = () => {
     
     
             <div>
-              <h3 className='font-semibold text-lg' style={{color:"#3F4646"}}>Colours Available </h3>
-              <div className='my-5'>
+              <h3 className='font-semibold text-base lg:text-lg' style={{color:"#3F4646"}}>Colours Available </h3>
+              <div className='my-4 lg:my-5'>
               
               {
              singleProduct.color !== undefined && singleProduct.color.length !==0 && singleProduct.color?
@@ -150,46 +150,46 @@ const Page = () => {
     
     
             <div>
-              <h3 className='font-semibold text-lg' style={{color:"#3F4646"}}>Amount</h3>
-              <div className='my-5'>
+              <h3 className='font-semibold text-base lg:text-lg' style={{color:"#3F4646"}}>Amount</h3>
+              <div className='my-4 lg:my-5'>
               <FaMinus className='inline' onClick={setDecrease}></FaMinus>
-              <span>{amount}</span>
+              <span className='mx-4'>{amount}</span>
               <FaPlus className='inline' onClick={setIncrease}></FaPlus>
     
     </div>
             </div>
     
-              <div className='w-[50%] grid grid-cols-2 gap-2'>
+              <div className='w-[100%] xl:w-[80%] 2xl:w-[50%] grid grid-cols-2 gap-2'>
                 <button className='font-semibold bg-[#8A33FD] py-3 rounded-md' style={{color:"fff"}} onClick={handleCart}>
                   <PiShoppingCartSimpleBold className='inline mr-2' style={{color:"fff"}} size={24}></PiShoppingCartSimpleBold>
-                 <span style={{color:"white"}}>Add to cart</span>
+                 <span style={{color:"white"}} className='text-lg'>Add to cart</span>
                   </button>
-                <button className='font-bold  py-2 text-lg border border-[#3C4242] rounded-md'>${singleProduct.discount_price==0?singleProduct.actual_price:singleProduct.discount_price}</button>
+                <button className='font-bold   py-2 text-lg border border-[#3C4242] rounded-md'>$63.00</button>
               </div>
     
-              <hr className='border border-[#BEBCBD] my-12'/>
+              <hr className='border border-[#BEBCBD] my-10 lg:my-12'/>
     
     
               <div>
-                <div className='grid grid-cols-2 gap 5'>
+                <div className='grid grid-cols-1 gap 5 lg:grid-cols-2'>
                     <div>
                       <RiSecurePaymentFill className='inline mr-5' style={{color:"#3C4242"}}></RiSecurePaymentFill>
-                      <p className='inline' style={{color:"#3C4242"}}>Secure payment</p>
+                      <p className='inline text-base' style={{color:"#3C4242"}}>Secure payment</p>
                     </div>
                     <div>
                       <GiTShirt className='inline mr-5' style={{color:"#3C4242"}}></GiTShirt>
-                      <p className='inline' style={{color:"#3C4242"}}>Size & Fit</p>
+                      <p className='inline text-base' style={{color:"#3C4242"}}>Size & Fit</p>
                     </div>
                 </div>
     
-                <div className='grid grid-cols-2 gap 5'>
+                <div className='grid grid-cols-1 gap 5 lg:grid-cols-2 '>
                     <div>
                       <LiaShippingFastSolid className='inline mr-5' style={{color:"#3C4242"}}></LiaShippingFastSolid>
-                      <p className='inline' style={{color:"#3C4242"}}>Free shipping</p>
+                      <p className='inline text-base' style={{color:"#3C4242"}}>Free shipping</p>
                     </div>
                     <div>
                       <RiArrowUpDownFill className='inline mr-5' style={{color:"#3C4242"}}></RiArrowUpDownFill>
-                      <p className='inline' style={{color:"#3C4242"}}>Free Shipping & Returns</p>
+                      <p className='inline text-base' style={{color:"#3C4242"}}>Free Shipping & Returns</p>
                     </div>
                 </div>
               </div>
@@ -205,4 +205,4 @@ const Page = () => {
   
 }
 
-export default Page
+export default page
