e5325f36b4d9094f31e3b64d8e02004404bab126
fenil-solanki <114174167+fenil-solanki@users.noreply.github.com>
2023-09-01 11:11:59 +0530

    Update Companies.tsx

--- client/src/app/component/Companies.tsx
+++ client/src/app/component/Companies.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { useFilterContext } from '@/context/FilterContext'
 import React from 'react'
 import {MdOutlineArrowForwardIos} from 'react-icons/md'
@@ -7,10 +6,15 @@ const Companies = ({company}:any) => {
     const {updateFilterValue}=useFilterContext()
     const capitalcompany = company.charAt(0).toUpperCase() + company.slice(1);
   return (
-    <button name="company" value={company} className="w-full font-semibold text-left py-1 grid grid-cols-4 text-xs sm:text-sm sm:py-2 md:text-base lg:text-xl" style={{color:"#8A8989"}} onClick={updateFilterValue}>
-    <span className='col-span-3'>{capitalcompany}</span>
-    <MdOutlineArrowForwardIos className='inline text-right'></MdOutlineArrowForwardIos>
-    </button>
+
+
+<div className='w-full  grid grid-cols-4 py-1 sm:py-2 '>
+<button name="company" value={company} className="col-span-3 font-semibold text-left text-xs sm:text-sm md:text-base lg:text-xl" style={{color:"#8A8989"}} onClick={updateFilterValue}>
+{capitalcompany}
+</button>
+<MdOutlineArrowForwardIos className='inline text-right font-semibold text-xs sm:text-sm md:text-base  lg:text-xl' style={{color:"#8A8989"}} ></MdOutlineArrowForwardIos>
+
+</div>
   )
 }
 
