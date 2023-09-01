246157ef29a2888102576f749603db6ffa414501
fenil-solanki <114174167+fenil-solanki@users.noreply.github.com>
2023-09-01 17:43:12 +0530

    Update CartItem.tsx

--- client/src/app/component/CartItem.tsx
+++ client/src/app/component/CartItem.tsx
@@ -1,6 +1,4 @@
-"use client"
-import React from 'react'
-import {useState} from 'react'
+import React,{useState} from 'react'
 import {RiDeleteBinLine} from 'react-icons/ri'
 import {FaPlus} from 'react-icons/fa'
 import {FaMinus} from 'react-icons/fa'
@@ -34,19 +32,23 @@ const CartItem = ({id,image,name,price,color,amount,stock,size}:any) => {
     <tr className='border-t-2 border-[#BEBCBD]'>
       <td className='text-center flex flex-row'>
         <div className='basis-2/6'>
-        <img src={image} className='w-[105px] h-[120px] rounded-md inline'></img>
+        <img src={image} className='w-[75px] h-[85px] rounded-md inline sm:w-[105px] sm:h-[120px]'></img>
 
         </div>
         <div className='basis-4/6 text-left'>
-        <p className='decoration-[#3C4242] font-bold'>{name}</p>
-        <p className='rounded-full h-6 w-6 mx-2 border border-[#000]' style={{backgroundColor:color}}></p>
-        <p className=''>Size : {size}</p>
+          <div className='ml-2'>
+
+        <p className='decoration-[#3C4242] font-semibold text-sm sm:text-lg md:text-xl md:font-bold'>{name}</p>
+        <p className='rounded-full my-2 mx-0.5  border border-[#000] h-4 w-4 sm:mx-2 sm:h-5 sm:w-5 md:h-6 md:w-6' style={{backgroundColor:color}}></p>
+        <p className=' font-semibold text-sm sm:text-lg md:text-xl md:font-bold'>Size : {size}</p>
+        </div>
+
         </div>
         </td>
-      <td className='text-center'>{price}</td>
-      <td className='text-center'><FaMinus className='inline' onClick={setDecrease}></FaMinus>{productAmount}<FaPlus className='inline' onClick={setIncrease}></FaPlus></td>
-      <td className='text-center'>Free</td>
-      <td className='text-center'>{productAmount*price}</td>
+      <td className='text-center hidden md:table-cell'>{price}</td>
+      <td className='text-center'><FaMinus className='inline' onClick={setDecrease}></FaMinus><span className='mx-1 text-lg sm:mx-4 sm:text-xl'>{productAmount}</span><FaPlus className='inline' onClick={setIncrease}></FaPlus></td>
+      <td className='text-center hidden md:table-cell'>Free</td>
+      <td className='text-center hidden md:table-cell'>{productAmount*price}</td>
       <td className='text-center'><RiDeleteBinLine className='inline' onClick={()=>removeItem(id)}></RiDeleteBinLine></td>
     </tr>
   )
