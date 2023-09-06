f1f873f0ac3f5e67771027aaeb1ab210ae3d0c4e
fenil-solanki <114174167+fenil-solanki@users.noreply.github.com>
2023-09-06 18:02:59 +0530

    Update Footer.tsx

--- client/src/app/component/Footer.tsx
+++ client/src/app/component/Footer.tsx
@@ -3,58 +3,58 @@ import {BiLogoFacebook, BiLogoTwitter,BiLogoInstagram,BiLogoLinkedin} from 'reac
 
 const Footer = () => {
   return (
-    <footer className='top-full sticky'>
+    <footer className='top-full sticky hidden xl:block'>
     <div className='p-8 bg-[#3C4242]' style={{color:"white"}}>
         
             <div className='px-24 py-8 container mx-auto'>
                 <div className='grid grid-cols-5'>
                     <div>
                         <h3 className='font-bold text-3xl'>Need Help</h3>
-                        <ul>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Contact Us</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Track Order</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Returns & Refunds</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>FAQ&aposs</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Career</li>
+                        <ul className='py-3'>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Contact Us</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Track Order</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Returns & Refunds</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>FAQ's</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Career</li>
                         </ul>
                     </div>
                     <div>
                         <h3 className='font-bold text-3xl'>Company</h3>
-                        <ul>
+                        <ul className='py-3'>
                             
-                        <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Contact Us</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Track Order</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Returns & Refunds</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>FAQ&aposs</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Career</li>
+                        <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Contact Us</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Track Order</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Returns & Refunds</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>FAQ's</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Career</li>
                         </ul>
 
                     </div>
                     <div>
                         <h3 className='font-bold text-3xl'>More Info</h3>
-                        <ul>
-                        <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Track Order</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Returns & Refunds</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>FAQ&aposs</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Career</li>
+                        <ul className='py-3'>
+                        <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Track Order</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Returns & Refunds</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>FAQ's</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Career</li>
                             </ul>
                     </div>
                     <div className='col-span-2'>
                     <h3 className='font-bold text-3xl'>Location</h3>
-                        <ul>
-                        <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>support@euphoria.in</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>Eklingpura Chouraha, Ahmedabad Main Road</li>
-                            <li className="font-medium text-lg" style={{color:"#F6F6F6"}}>(NH 8- Near Mahadev Hotel) Udaipur, India- 313002</li>
+                        <ul className='py-3'>
+                        <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>support@euphoria.in</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>Eklingpura Chouraha, Ahmedabad Main Road</li>
+                            <li className="my-5 font-medium text-lg" style={{color:"#F6F6F6"}}>(NH 8- Near Mahadev Hotel) Udaipur, India- 313002</li>
                             </ul>
                     </div>
                 </div>
 
                     <div className='grid grid-cols-2'>
                         <div>
-                            <button><BiLogoFacebook></BiLogoFacebook></button>
-                            <button><BiLogoInstagram></BiLogoInstagram></button>
-                            <button><BiLogoTwitter></BiLogoTwitter></button>
-                            <button><BiLogoLinkedin></BiLogoLinkedin></button>
+                            <button className='h-[37px] w-[37px] bg-[#fff] mx-2 rounded-lg'><BiLogoFacebook className='mx-auto' size={20} style={{color:"#2A2F2F"}}></BiLogoFacebook></button>
+                            <button className='h-[37px] w-[37px] bg-[#fff] mx-2 rounded-lg'><BiLogoInstagram className='mx-auto' size={20} style={{color:"#2A2F2F"}}></BiLogoInstagram></button>
+                            <button className='h-[37px] w-[37px] bg-[#fff] mx-2 rounded-lg'><BiLogoTwitter className='mx-auto' size={20} style={{color:"#2A2F2F"}}></BiLogoTwitter></button>
+                            <button className='h-[37px] w-[37px] bg-[#fff] mx-2 rounded-lg'><BiLogoLinkedin className='mx-auto' size={20} style={{color:"#2A2F2F"}}></BiLogoLinkedin></button>
                         </div>
                         <div>
                                 <h3>Download The App</h3>
